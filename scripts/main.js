c4e2d96200cddf1a83a8f72d36c3394d200cab69
Suada Haji <suada.haji@andela.com>
2017-09-23 23:38:35 +0300

    Add row select

--- scripts/main.js
+++ scripts/main.js
@@ -23,29 +23,51 @@
  var lastName = document.getElementById('inputLName');
  var cohort = document.getElementById('selectCohort');
  var editBtn = document.getElementById('editButton');
- var tableStaff = $('#listStaff').DataTable();
+ var rootRef = firebase.database().ref().child("staff");
+//  var tableStaff = $('#listStaff').DataTable({
+//     select: true
+//  });
  var tablle = document.getElementById('table-body');
  var currentUID;
 
 
- function addNewStaff(fname, sname, lname, cohort) {
-    
+ function addNewStaff(fname, sname, lname, cohort, newStaffKey) {
+     if(!newStaffKey) {
 
-        // Get key for new staff member
-        var newStaffKey = firebase.database().ref().child('staff').push().key;
-
-        var postData = {
+        newStaffKey = firebase.database().ref().child('staff').push().key;
+        
+                var postData = {
+                    id: newStaffKey,
+                    fname: fname,
+                    sname: sname,
+                    lname: lname,
+                    cohort: cohort
+                   };
+        
+                var updates = {};
+                updates['/staff/' + newStaffKey] = postData;
+        
+                return firebase.database().ref().update(updates);
+     } else {
+        firebase.database().ref('staff/' + newStaffKey).set({
             id: newStaffKey,
             fname: fname,
             sname: sname,
             lname: lname,
             cohort: cohort
-           };
+          }).then(function() { // removed from Firebase DB
+            console.log("Update succeeded.")
+           })
+           .catch(function(error) {
+            console.log("Update failed: " + error.message)
+           });
 
-        var updates = {};
-        updates['/staff/' + newStaffKey] = postData;
+        console.log("Has key");
+       
+     }
 
-        return firebase.database().ref().update(updates);
+    // Get key for new staff member
+   
  }
 
  function getStaff() {
@@ -59,6 +81,27 @@
      cohort.value = cohort;
  }
 
+ function submitForm(newStaffKey) {
+    messageForm.onsubmit = function(e) {
+        e.preventDefault();
+        var fn = firstName.value;
+        var ln = lastName.value;
+        var sn = surName.value;
+        var ch = cohort.value;
+
+        if(fn && ln && sn && ch) {
+            addNewStaff(fn, sn, ln, ch, newStaffKey);
+            var fn = '';
+            var ln = '';
+            var sn = '';
+            var ch = '';
+            messageForm.reset();
+            $('#addStaffModal').modal('hide');
+            window.location.reload();        
+        }
+    };
+ }
+
  // Binding
 $('document').ready(()=> {
      // Saves staff on form submit
@@ -70,46 +113,106 @@ $('document').ready(()=> {
         $('#addStaffModal').modal('hide');        
      })
     
-     messageForm.onsubmit = function(e) {
-        e.preventDefault();
-        var fn = firstName.value;
-        var ln = lastName.value;
-        var sn = surName.value;
-        var ch = cohort.value;
-
-        if(fn && ln && sn && ch) {
-            addNewStaff(fn, sn, ln, ch);
-            var fn = '';
-            var ln = '';
-            var sn = '';
-            var ch = '';
-            messageForm.reset();
-            $('#addStaffModal').modal('hide');        
-        }
-    };
+     submitForm();
     var count = 0;
+
     
-    getStaff().on('child_added', function(snapshot) {
+
+
+
+    
+    rootRef.orderByChild("fname").on('child_added', function(snapshot) {
         count++;
        
        if(snapshot.exists()) {
            var content = '';
            var val = snapshot.val();
            if(val.fname) {
-               var dataset = [count, val.fname, val.sname,val.lname, val.cohort];
-           } else {
-               var dataset = [count, "", " ", " ", " ", " ", " "];
-           }
-           
-           tableStaff.rows.add([dataset]).draw();
-           currentUID = snapshot.key;
-   
+            var assetKey = snapshot.child("id").val();
+            var fname = snapshot.child("fname").val();
+            var sname = snapshot.child("sname").val();
+            var lname = snapshot.child("lname").val();
+            var cohort = snapshot.child("cohort").val();
+            $("#listStaff").append("<tr id='row"+assetKey+"' class='clickable-row' key='"+ assetKey +"'>"+
+                                    "<td class='mdl-data-table__cell--non-numeric'>" + fname + "</td>" +
+                                    "<td class='mdl-data-table__cell--non-numeric'>" + sname + "</td>" +
+                                    "<td class='mdl-data-table__cell--non-numeric'>" + lname + "</td>" +
+                                    "<td class='mdl-data-table__cell--non-numeric'>" + cohort + "</td>" +
+      
+                                    "<td class='mdl-data-table__cell--non-numeric'><div buttons>"+
+                                    "<button key='"+ assetKey +"' class='edit_btn btn btn-primary my-2 my-sm-0 update-staff' ><i class='fa fa-pencil' aria-hidden='true'></i></button>"+" "+
+                                            "<button key='"+ assetKey +"' class='delete-btn btn btn-danger my-2 my-sm-0'><i class='fa fa-trash' aria-hidden='true'></i></button>"+" "+
+                                            "</div></td></tr>");
+           } 
        }
    });
+
+   rootRef.orderByChild("fname").on("child_changed", snapshot => {
+    var val = snapshot.val();
+    if(val.fname) {
+        var assetKey = snapshot.child("id").val();
+        var fname = snapshot.child("fname").val();
+        var sname = snapshot.child("sname").val();
+        var lname = snapshot.child("lname").val();
+        var cohort = snapshot.child("cohort").val();
+        console.log(fname);
+  
+        $("#listStaff").append("<tr id='row"+assetKey+"' class='clickable-row' key='"+ assetKey +"'>"+
+                                "<td class='mdl-data-table__cell--non-numeric'>" + fname + "</td>" +
+                                "<td class='mdl-data-table__cell--non-numeric'>" + sname + "</td>" +
+                                "<td class='mdl-data-table__cell--non-numeric'>" + lname + "</td>" +
+                                "<td class='mdl-data-table__cell--non-numeric'>" + cohort + "</td>" +
+  
+                                "<td class='mdl-data-table__cell--non-numeric'><div buttons>"+
+                                "<button key='"+ assetKey +"' class='edit_btn btn btn-outline-success my-2 my-sm-0 update-staff' ><i class='fa fa-pencil' aria-hidden='true'></i></button>"+" "+
+                                        "<button key='"+ assetKey +"' class='delete-btn btn btn-danger my-2 my-sm-0'><i class='fa fa-trash' aria-hidden='true'></i></button>"+" "+
+                                        "</div></td></tr>");
+       } 
        
+    });
+
+    rootRef.on('child_removed', function(snapshot) {
+        var key = snapshot.key;
+        $('#'+key).remove();
+    
+      });
+  
+});
+
+$('#listStaff').on('click', ".delete-btn", function(){ 
+var key = $(this).attr('key');
+var itemToRemove = rootRef.child(key);
+itemToRemove.remove()
+.then(function() { // removed from Firebase DB
+ console.log("Remove succeeded.")
 })
+.catch(function(error) {
+ console.log("Remove failed: " + error.message)
+});
 
+// window.location.reload();
 
+});
 
+$('#listStaff').on('click', ".edit_btn", function(){ 
+    var key = $(this).attr('key');
+    var itemToUpdate = rootRef.child(key);
+    firebase.database().ref("staff/" + key).once('value').then(function(snapshot) {
+        firstName.value = snapshot.val().fname;
+        surName.value = snapshot.val().sname;
+        lastName.value = snapshot.val().lname;
+        cohort.value = snapshot.val().cohort;
+      });
+      $('#addStaffModal').modal('show');
+      submitForm(key);
+    });
+
+    $("#listStaff").on("click", "tr", function () {
+        var col = $(this).index();
+        $(this).addClass("highlight").siblings().removeClass("highlight");
+        
+          console.log("OK");
+              
+     });
 
 
